e1364a9c8afd27de6cc1e5bb659cc36512156972
Meili0306 <A113221046@mail.shu.edu.tw>
2025-04-10 13:04:02 +0800

    114-04-10

---
+++ 114_04_10/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 5; // 陣列的大小
+            int[] lotteryNumbers = new int[SIZE]; // 用來儲存樂透號碼的陣列
+            Random rand = new Random(); 
+
+            for (int i = 0;i < lotteryNumbers.Length; i++)
+            {
+                //產生1~42的亂數，確認產生的亂數沒有與陣列中元素重複，在放入陣列中
+                int number;
+                do
+                {
+                    number = rand.Next(1, 43); // 產生1~42的亂數
+                } while (lotteryNumbers.Contains(number)); // 確認不重複
+                lotteryNumbers[i] = number; // 將不重複的亂數放入陣列中
+            }
+
+            //將lotteryNumbers 陣列中的數字由小到大排序
+            Array.Sort(lotteryNumbers); // 將lotteryNumbers 陣列中的數字顯示在Label上
+
+
+            //firstLabel.Text = lotteryNumbers[0].ToString();
+            //secondLabel.Text = lotteryNumbers[1].ToString();
+            //thirdLabel.Text = lotteryNumbers[2].ToString();
+            //fourthLabel.Text = lotteryNumbers[3].ToString();
+            //fifthLabel.Text = lotteryNumbers[4].ToString();
+
+            // 用迴圈來顯示樂透號碼
+            Label[] showLabels = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel };
+            for (int i = 0; i < showLabels.Length; i++)
+            {
+                showLabels[i].Text = lotteryNumbers[i].ToString();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ 114_04_10/Tutorial 7-2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Average 方法接受一個 int 陣列作為參數
+        // 並返回該陣列中所有值的平均值。
+        private double Average(int[] scores)
+        {
+            // 計算總和
+            int total = 0;
+            foreach (int score in scores)
+            {
+                total += score;
+            }
+            // 返回平均值
+            return (double)total / scores.Length;
+        }
+
+        // Highest 方法接受一個 int 陣列作為參數
+        // 並返回該陣列中的最大值。
+        private int Highest(int[] scores)
+        {
+            // 找到最大值
+            int highest = scores[0];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] > highest)
+                {
+                    highest = scores[i];
+                }
+            }
+            // 返回最大值
+            return highest;
+        }
+
+        // Lowest 方法接受一個 int 陣列作為參數
+        // 並返回該陣列中的最小值。
+        private int Lowest(int[] scores)
+        {
+            // 找到最小值
+            int lowest = scores[0];
+            foreach (int score in scores)
+            {
+                if (score < lowest)
+                {
+                    lowest = score;
+                }
+            }
+            // 返回最小值
+            return lowest;
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            // 這裡可以添加代碼來獲取分數並調用上述方法
+            const int SIZE = 48;
+            int[] testScores = new int[SIZE];
+            int index = 0;
+            int highestScore = 0;
+            int lowestScore = 0;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+            try
+            { 
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    // 開啟檔案
+                    inputFile = File.OpenText(openFile.FileName);
+                    // 讀取檔案中的分數
+                    while (!inputFile.EndOfStream && index < SIZE)
+                    {
+                        testScores[index] = Convert.ToInt32(inputFile.ReadLine());
+                        index++;
+                    }
+                    inputFile.Close();
+                    // 計算最高分、最低分和平均分
+                    highestScore = Highest(testScores);
+                    lowestScore = Lowest(testScores);
+                    averageScore = Average(testScores);
+                    // 顯示結果
+                    highScoreLabel.Text = "Highest Score: " + highestScore.ToString();
+                    lowScoreLabel.Text = "Lowest Score: " + lowestScore.ToString();
+                    averageScoreLabel.Text = "Average Score: " + averageScore.ToString("n1");
+                }
+                else
+                {
+                    MessageBox.Show("No file selected.", "Error");
+                    return;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
